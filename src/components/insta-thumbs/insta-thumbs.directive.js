a9c36d80799c862d6b50ec43cb949da9af6b3db3
Stanley Sathler <stanleysathlerpinto@gmail.com>
2017-11-07 11:07:35 -0200

    Atualizado Instagram

--- src/components/insta-thumbs/insta-thumbs.directive.js
+++ src/components/insta-thumbs/insta-thumbs.directive.js
@@ -13,12 +13,12 @@
 			replace: true,
 			link: function(theScope, theElement){
 				// Stanley: 421923937 | 421923937.1677ed0.8b7b578910ea4c08a063c82d882bffb
-				// Amanda Lima: 275648156 | 275648156.79e9056.9f4f7c27172d464bb5ea1d9bf927302d
+				// Amanda Lima: 275648156 | 275648156.79e9056.b488c6f91d1c451cb1df41ff57a6da44
 				var feed = new Instafeed({
 					get: "user",
 					userId: "275648156",
 					clientId: "79e905681316484b9fe108bbef5774fb",
-					accessToken: "275648156.79e9056.9f4f7c27172d464bb5ea1d9bf927302d",
+					accessToken: "275648156.79e9056.b488c6f91d1c451cb1df41ff57a6da44",
 					target: angular.element(theElement)[0],
 					sortBy: "most-recent",
 					limit: 6,
