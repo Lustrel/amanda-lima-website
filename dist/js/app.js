32a05b1ecd4007569586ff6ce2512daeadb14d8c
Stanley Sathler <stanleysathlerpinto@gmail.com>
2017-11-08 13:36:37 -0200

    .

--- dist/js/app.js
+++ dist/js/app.js
@@ -161,16 +161,31 @@ $(document).ready(function(){
 				var $overlay = $(".lst-menu-button__overlay");
 				var $body = $('body');
 
-				$scope.openMenu = function(){
-					console.log("Opening menu");
-					$overlay.css({ display: 'block' });
+				function showOverlay() {
+					$overlay.css({ 
+						display: 'block',
+						position: 'absolute',
+						left: 0,
+						right: 0,
+						top: 0,
+						bottom: 0
+					});
 					$body.css({ overflow: 'hidden' });
+				}
+
+				function hideOverlay() {
+					$overlay.css({ display: 'none' });
+					$body.css({ overflow: 'initial' });
+				}
+
+				$scope.openMenu = function(){
+					showOverlay();
+					window.addEventListener('resize', showOverlay);
 				};
 
 				$scope.closeMenu = function(){
-					console.log("Closing menu");
-					$overlay.css({ display: 'none' });
-					$body.css({ overflow: 'initial' });
+					hideOverlay();
+					window.removeEventListener('resize');
 				};
 			}
 		};
